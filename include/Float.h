0715b84e9f44253d9485a3a810d6b0297407bc56
Nils Hagner <nilshagner@tutanota.com>
2020-12-22 23:56:53 +0100

    Own Float3

--- include/Float.h
+++ include/Float.h
@@ -4,13 +4,24 @@
 
 #ifndef BACHELORTHESIS_FLOAT_H
 #define BACHELORTHESIS_FLOAT_H
-typedef float Float;
 
-class Float3 {
+template <typename T>
+class Vec3 {
+    T x;
+    T y;
+    T z;
 public:
-    Float x;
-    Float y;
-    Float z;
+    Vec3() = default;
+    Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
+    Vec3(std::initializer_list<T> & list) {
+        if (list.size() == 3) {
+            x = list[0];
+            y = list[1];
+            z = list[2];
+        }
+    }
 };
+typedef float Float;
+typedef Vec3<float> Float3;
 
 #endif //BACHELORTHESIS_FLOAT_H
