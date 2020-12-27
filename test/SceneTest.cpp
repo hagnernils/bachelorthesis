f97e9d6f092a32f219890b83e246597a5509f89d
Nils Hagner <nilshagner@tutanota.com>
2020-12-27 18:00:17 +0100

    Test Gltf loading

---
+++ test/SceneTest.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Nils Hagner on 27.12.20.
+//
+
+#include "gtest/gtest.h"
+#include "Scene.h"
+#include "../Scene.cpp"
+
+TEST(Scene, LoadGltf) {
+    Scene scene;
+    scene.loadGLTF("triangle.gltf");
+    auto prims = scene.objects[0].toPrimitives();
+
+    for (auto &prim : prims) {
+        std::cerr << prim << std::endl;
+    }
+
+    ASSERT_EQ(prims[0].a,       (Float3{-1,  0,  0}));
+    ASSERT_EQ(prims[0].b,       (Float3{ 1,  0,  0}));
+    ASSERT_EQ(prims[0].c,       (Float3{-1, -2,  0}));
+    ASSERT_EQ(prims[0].normal,  (Float3{ 0,  0, -1}));
+}
