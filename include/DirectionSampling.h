93285e83fba1977ce3c9fab0f6ab6a4c7ff3ea0e
Nils Hagner <nilshagner@tutanota.com>
2021-02-17 21:32:34 +0100

    Uniform sampling along normal

--- include/DirectionSampling.h
+++ include/DirectionSampling.h
@@ -26,6 +26,19 @@ inline Float3 uniformSampleHemisphere(const Point2f &p) {
     return Float3(scale * std::cos(phi), scale * std::sin(phi), z);
 }
 
+inline Float3 uniformSampleHemisphereAtNormal(const Float3 &surfaceNormal, const Point2f &p) {
+    Float3 sample = uniformSampleHemisphere(p);
+    // create an orthonormal basis (s|t|n) of current surface point
+    // see https://raytracing.github.io/books/RayTracingTheRestOfYourLife.html#orthonormalbases/generatinganorthonormalbasis
+    Float3 s, t;
+    Float3 a = fabs(surfaceNormal.x) > 0.9 ? Float3 (0,1,0) : Float3 (1,0,0);
+    t = Float3::cross(surfaceNormal, a);
+    t.normalize();
+    s = Float3::cross(surfaceNormal, t);
+
+    return sample.x * s + sample.y * t + sample.z * surfaceNormal;
+}
+
 constexpr Float uniformHemispherePdf() { return 2 * M_1_PI; }
 
 inline Float cosineHemispherePdf(Float angleToNormal) { return angleToNormal * M_1_PI; }
