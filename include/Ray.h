4c975e88a95084f6f3d152d2960d890e01e33c2f
Nils Hagner <nilshagner@tutanota.com>
2021-02-17 21:49:25 +0100

    Add energy to rays

--- include/Ray.h
+++ include/Ray.h
@@ -11,10 +11,13 @@ struct Ray {
     Float3 base;
     Float3 dir;
     Float time = 0;
+    // TODO: maybe call this intensity bc it is along the differential area normal to the ray?
+    Float energy = 0;
 
     Ray() = default;
 
     Ray(Float3 base, Float3 dir) : base(base), dir(dir) {}
+    Ray(Float3 base, Float3 dir, Float energy) : base(base), dir(dir), energy(energy) {}
 
     Float3 at(Float t) const { return base + t * dir; }
 };
